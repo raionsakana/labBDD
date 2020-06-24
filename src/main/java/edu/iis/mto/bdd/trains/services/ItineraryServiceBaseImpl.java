ff87fb51006c00fe124da33db92b9849fd63ff5b
Patryk Janowski <pjanowski97@gmail.com>
2020-06-24 19:04:51 +0200

    feature1_impl ItineraryServiceBaseImpl implemented

--- src/main/java/edu/iis/mto/bdd/trains/services/ItineraryServiceBaseImpl.java
+++ src/main/java/edu/iis/mto/bdd/trains/services/ItineraryServiceBaseImpl.java
@@ -1,18 +1,34 @@
 package edu.iis.mto.bdd.trains.services;
 
+import edu.iis.mto.bdd.trains.model.Line;
 import org.joda.time.LocalTime;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItineraryServiceBaseImpl implements ItineraryService {
 
-    ItineraryServiceBaseImpl() {
+    private TimetableService timetableService;
+    private int restTime;
 
+    ItineraryServiceBaseImpl(TimetableService timetableService, int restTime) {
+        this.timetableService = timetableService;
+        this.restTime = restTime;
     }
 
     @Override
     public List<LocalTime> findNextDepartures(String departure, String destination, LocalTime startTime) {
-        return null;
+        List<LocalTime> foundDepartures = new ArrayList<>();
+
+        for (Line line : timetableService.findLinesThrough(departure, destination)) {
+            for (LocalTime localTime : timetableService.findArrivalTimes(line, departure)) {
+                if (startTime.isBefore(localTime) && startTime.plusMinutes(restTime).isAfter(localTime)) {
+                    foundDepartures.add(localTime);
+                }
+            }
+        }
+
+        return foundDepartures;
     }
 
 }
